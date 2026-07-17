6c41b0bc5a348d42d68cdbee4ff546688fdb14db
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2026-07-17 16:00:18 +0400

    plan-success-goes-to-apply
    * trigger terraform-plan
    
    * test trigger for plan
    
    * fix: pipeline debug #1
    
    * fix: fmt issue fixed

--- infra/envs/poc/eks.tf
+++ infra/envs/poc/eks.tf
@@ -2,13 +2,13 @@ module "eks_gateway" {
   source = "../../modules/eks"
 
   cluster_name                 = "eks-gateway"
-  kubernetes_version            = var.kubernetes_version
-  cluster_role_arn              = module.iam_eks_gateway.cluster_role_arn
-  node_role_arn                 = module.iam_eks_gateway.node_role_arn
-  private_subnet_ids            = module.vpc_gateway.private_subnet_ids
-  endpoint_public_access        = true
-  endpoint_public_access_cidrs  = var.eks_public_access_cidrs
-  instance_types                = var.node_instance_types
+  kubernetes_version           = var.kubernetes_version
+  cluster_role_arn             = module.iam_eks_gateway.cluster_role_arn
+  node_role_arn                = module.iam_eks_gateway.node_role_arn
+  private_subnet_ids           = module.vpc_gateway.private_subnet_ids
+  endpoint_public_access       = true
+  endpoint_public_access_cidrs = var.eks_public_access_cidrs
+  instance_types               = var.node_instance_types
 
   tags = { Layer = "gateway" }
 }
@@ -17,13 +17,13 @@ module "eks_backend" {
   source = "../../modules/eks"
 
   cluster_name                 = "eks-backend"
-  kubernetes_version            = var.kubernetes_version
-  cluster_role_arn              = module.iam_eks_backend.cluster_role_arn
-  node_role_arn                 = module.iam_eks_backend.node_role_arn
-  private_subnet_ids            = module.vpc_backend.private_subnet_ids
-  endpoint_public_access        = true
-  endpoint_public_access_cidrs  = var.eks_public_access_cidrs
-  instance_types                = var.node_instance_types
+  kubernetes_version           = var.kubernetes_version
+  cluster_role_arn             = module.iam_eks_backend.cluster_role_arn
+  node_role_arn                = module.iam_eks_backend.node_role_arn
+  private_subnet_ids           = module.vpc_backend.private_subnet_ids
+  endpoint_public_access       = true
+  endpoint_public_access_cidrs = var.eks_public_access_cidrs
+  instance_types               = var.node_instance_types
 
   tags = { Layer = "backend" }
 }
--- infra/envs/poc/terraform.tfvars
+++ infra/envs/poc/terraform.tfvars
@@ -4,8 +4,8 @@ azs        = ["us-east-2a", "us-east-2b"]
 gateway_vpc_cidr = "10.0.0.0/16"
 backend_vpc_cidr = "10.1.0.0/16"
 
-# Tighten this to GitHub Actions runner IP ranges + your own IP for real use.
 eks_public_access_cidrs = ["0.0.0.0/0"]
 
-kubernetes_version   = "1.35"
-node_instance_types  = ["t3.medium"]
+kubernetes_version  = "1.35"
+node_instance_types = ["t3.medium"]
+
